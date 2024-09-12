605ab2e285f49ae5169a29818f54283177b7c5ce
Omar Zunic <contact@omarzunic.com>
2024-09-12 19:14:34 +0200

    mobile fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "nrush",
@@ -17,17 +17,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-	                                   children,
-                                   }: Readonly<{
+	children,
+}: Readonly<{
 	children: React.ReactNode;
 }>) {
 	return (
 		<html lang="en">
-		<body className={inter.className}>
-		<Script defer data-domain="nrush.omarzunic.com" src="https://analytics.omarzunic.com/js/script.js"></Script>
-		<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet"/>
-		{children}
-		</body>
+			<body className={inter.className}>
+				<Script defer data-domain="nrush.omarzunic.com" src="https://analytics.omarzunic.com/js/script.js"></Script>
+				{children}
+			</body>
 		</html>
 	);
 }
--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,17 +1,19 @@
 'use client'
 
-import React, {useMemo, useRef, useState} from 'react'
-import {Canvas, useFrame, useThree} from '@react-three/fiber'
+import React, { useMemo, useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
-import {ScrollArea} from "@/components/ui/scroll-area"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { ScrollArea } from "@/components/ui/scroll-area"
 import Link from "next/link";
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ["latin"] });
 
 const ParticleText = () => {
 	const textRef = useRef<THREE.Points>(null)
 	const particlesCount = 5000 // Increased particle count for larger text
 
-	const {positions, originalPositions} = useMemo(() => {
+	const { positions, originalPositions } = useMemo(() => {
 		const positions = new Float32Array(particlesCount * 3)
 		const originalPositions = new Float32Array(particlesCount * 3)
 		const canvas = document.createElement('canvas')
@@ -19,7 +21,7 @@ const ParticleText = () => {
 		canvas.width = 1024
 		canvas.height = 256
 		ctx.fillStyle = '#ffffff'
-		ctx.font = 'bold 200px Inter'
+		ctx.font = `bold 200px ${inter.style.fontFamily}`
 		ctx.textAlign = 'center'
 		ctx.textBaseline = 'middle'
 		ctx.fillText('nrush', canvas.width / 2, canvas.height / 2)
@@ -50,7 +52,7 @@ const ParticleText = () => {
 			index += 3
 		}
 
-		return {positions, originalPositions}
+		return { positions, originalPositions }
 	}, [])
 
 	useFrame(() => {
@@ -78,7 +80,7 @@ const ParticleText = () => {
 					itemSize={3}
 				/>
 			</bufferGeometry>
-			<pointsMaterial size={0.01} color="#ffffff" sizeAttenuation={true}/>
+			<pointsMaterial size={0.01} color="#ffffff" sizeAttenuation={true} />
 		</points>
 	)
 }
@@ -91,8 +93,8 @@ export function Content() {
 			<div
 				className="h-[15dvh] md:h-[25dvh] w-screen mb-12 pt-4"
 			>
-				<Canvas camera={{position: [0, 0, 1.2], fov: 20}}>
-					<ParticleText/>
+				<Canvas camera={{ position: [0, 0, 1.2], fov: 20 }}>
+					<ParticleText />
 				</Canvas>
 			</div>
 
@@ -102,75 +104,75 @@ export function Content() {
 				<div className="flex justify-center space-x-4 mb-8">
 					<Link href="https://www.npmjs.com/package/nrush" className="flex items-center">
 						<img src="https://img.shields.io/npm/dw/nrush?style=for-the-badge&logo=npm" alt="npm downloads"
-						     className="h-6"/>
+							className="h-6" />
 					</Link>
 					<Link href="https://github.com/omznc/nrush" className="flex items-center">
 						<img
 							src="https://img.shields.io/github/actions/workflow/status/omznc/nrush/cd.yml?style=for-the-badge&logo=github"
-							alt="GitHub status" className="h-6"/>
+							alt="GitHub status" className="h-6" />
 					</Link>
 				</div>
 
 				<Tabs value={activeTab} onValueChange={setActiveTab} className="mb-12">
 					<TabsList className="grid w-full grid-cols-4 bg-white/10 ">
 						<TabsTrigger value="usage"
-						             className="text-white data-[state=active]:bg-neutral-200">Usage</TabsTrigger>
+							className="text-white text-xs md:text-md data-[state=active]:bg-neutral-200">Usage</TabsTrigger>
 						<TabsTrigger value="commands"
-						             className="text-white data-[state=active]:bg-neutral-200">Commands</TabsTrigger>
+							className="text-white text-xs md:text-md data-[state=active]:bg-neutral-200">Commands</TabsTrigger>
 						<TabsTrigger value="purpose"
-						             className="text-white data-[state=active]:bg-neutral-200">Purpose</TabsTrigger>
+							className="text-white text-xs md:text-md data-[state=active]:bg-neutral-200">Purpose</TabsTrigger>
 						<TabsTrigger value="contribute"
-						             className="text-white data-[state=active]:bg-neutral-200">Contribute</TabsTrigger>
+							className="text-white text-xs md:text-md data-[state=active]:bg-neutral-200">Contribute</TabsTrigger>
 					</TabsList>
 					<TabsContent value="usage" className="mt-6 slide-down">
 						<h2 className="text-2xl font-semibold mb-4">Usage</h2>
-						<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+						<div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-4">
 							<div>
 								<h3 className="text-xl font-semibold mb-2">Bun</h3>
 								<pre id={'prebun'}
-								     className="relative bg-neutral-900 h-full p-4 rounded-lg overflow-x-auto cursor-pointer"
-								     onClick={
-									     () => {
-										     navigator.clipboard.writeText('bun --bun add -d nrush@latest\nnrush -i')
-										     const prebun = document.getElementById('prebun')
-										     const overlay = document.createElement('div')
-										     overlay.classList.add('absolute', 'top-0', 'left-0', 'w-full', 'h-full', 'bg-black/50', 'flex', 'justify-center', 'items-center', 'text-white', 'text-sm', 'font-mono', 'z-10')
-										     overlay.innerText = 'copied to clipboard'
-										     prebun?.appendChild(overlay)
+									className="relative bg-neutral-900 md:h-full p-4 rounded-lg overflow-x-auto cursor-pointer"
+									onClick={
+										() => {
+											navigator.clipboard.writeText('bun --bun add -d nrush@latest\nnrush -i')
+											const prebun = document.getElementById('prebun')
+											const overlay = document.createElement('div')
+											overlay.classList.add('absolute', 'top-0', 'left-0', 'w-full', 'h-full', 'bg-black/50', 'flex', 'justify-center', 'items-center', 'text-white', 'text-sm', 'font-mono', 'z-10')
+											overlay.innerText = 'copied to clipboard'
+											prebun?.appendChild(overlay)
 
-										     setTimeout(() => {
-											     prebun?.removeChild(overlay)
-										     }, 500)
-									     }
-								     }>
-				                  <code>
-				                    bun --bun add -d nrush@latest{'\n'}
-					                  nrush -i
-				                  </code>
-				                </pre>
+											setTimeout(() => {
+												prebun?.removeChild(overlay)
+											}, 500)
+										}
+									}>
+									<code>
+										bun --bun add -d nrush@latest{'\n'}
+										nrush -i
+									</code>
+								</pre>
 							</div>
 							<div>
 								<h3 className="text-xl font-semibold mb-2">Node</h3>
 								<pre id={'prenode'}
-								     className="relative bg-neutral-900 h-full p-4 rounded-lg overflow-x-auto cursor-pointer"
-								     onClick={
-									     () => {
-										     navigator.clipboard.writeText('npx nrush@latest -i')
-										     const prenode = document.getElementById('prenode')
-										     const overlay = document.createElement('div')
-										     overlay.classList.add('absolute', 'top-0', 'left-0', 'w-full', 'h-full', 'bg-black/50', 'flex', 'justify-center', 'items-center', 'text-white', 'text-sm', 'font-mono', 'z-10')
-										     overlay.innerText = 'copied to clipboard'
-										     prenode?.appendChild(overlay)
+									className="relative bg-neutral-900 md:h-full p-4 rounded-lg overflow-x-auto cursor-pointer"
+									onClick={
+										() => {
+											navigator.clipboard.writeText('npx nrush@latest -i')
+											const prenode = document.getElementById('prenode')
+											const overlay = document.createElement('div')
+											overlay.classList.add('absolute', 'top-0', 'left-0', 'w-full', 'h-full', 'bg-black/50', 'flex', 'justify-center', 'items-center', 'text-white', 'text-sm', 'font-mono', 'z-10')
+											overlay.innerText = 'copied to clipboard'
+											prenode?.appendChild(overlay)
 
-										     setTimeout(() => {
-											     prenode?.removeChild(overlay)
-										     }, 500)
-									     }
-								     }>
-				                  <code>
-				                    npx nrush@latest -i
-				                  </code>
-				                </pre>
+											setTimeout(() => {
+												prenode?.removeChild(overlay)
+											}, 500)
+										}
+									}>
+									<code>
+										npx nrush@latest -i
+									</code>
+								</pre>
 							</div>
 						</div>
 					</TabsContent>
@@ -179,8 +181,8 @@ export function Content() {
 						<ScrollArea className="h-[400px] rounded-md border border-neutral-800 p-4">
 							<h3 className="text-xl font-semibold mb-2">Commands:</h3>
 							<ul className="list-disc pl-6 mb-4">
-								<li><code>nrush about</code> - Display comprehensive information about NRush.</li>
-								<li><code>nrush help</code> - Provide a usage guide for NRush.</li>
+								<li><code>nrush about</code> - Display comprehensive information about nrush.</li>
+								<li><code>nrush help</code> - Provide a usage guide for nrush.</li>
 							</ul>
 							<h3 className="text-xl font-semibold mb-2">Arguments:</h3>
 							<ul className="list-disc pl-6">
@@ -242,23 +244,23 @@ export function Content() {
 						<summary className="cursor-pointer font-semibold">Underlying binaries</summary>
 						<ul className="list-disc pl-6 mt-2">
 							<li><a href="https://www.npmjs.com/package/nrush-windows-x64"
-							       className="text-blue-400 hover:underline">nrush-windows-x64</a></li>
+								className="text-blue-400 hover:underline">nrush-windows-x64</a></li>
 							<li><a href="https://www.npmjs.com/package/nrush-windows-arm64"
-							       className="text-blue-400 hover:underline">nrush-windows-arm64</a></li>
+								className="text-blue-400 hover:underline">nrush-windows-arm64</a></li>
 							<li><a href="https://www.npmjs.com/package/nrush-darwin-x64"
-							       className="text-blue-400 hover:underline">nrush-darwin-x64</a></li>
+								className="text-blue-400 hover:underline">nrush-darwin-x64</a></li>
 							<li><a href="https://www.npmjs.com/package/nrush-darwin-arm64"
-							       className="text-blue-400 hover:underline">nrush-darwin-arm64</a></li>
+								className="text-blue-400 hover:underline">nrush-darwin-arm64</a></li>
 							<li><a href="https://www.npmjs.com/package/nrush-linux-x64"
-							       className="text-blue-400 hover:underline">nrush-linux-x64</a></li>
+								className="text-blue-400 hover:underline">nrush-linux-x64</a></li>
 							<li><a href="https://www.npmjs.com/package/nrush-linux-arm64"
-							       className="text-blue-400 hover:underline">nrush-linux-arm64</a></li>
+								className="text-blue-400 hover:underline">nrush-linux-arm64</a></li>
 						</ul>
 					</details>
 				</div>
 
 				<Link href="https://omarzunic.com?utm_source=nrush"
-				      className="mt-12 flex opacity-30 hover:opacity-100 transition-all items-center justify-center">
+					className="mt-12 flex opacity-30 hover:opacity-100 transition-all items-center justify-center">
 					with ❤️ by
 					omznc
 				</Link>
